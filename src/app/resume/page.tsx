f6aa50875e76540e02749ffa47bfd623b3afb9da
J. Alejandro Coronado Cuesta <tk.jalejandro@gmail.com>
2023-10-24 18:52:28 +0200

    Sonia resume was created as an object and ts typed.

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,13 @@
+import Resume from "@/modules/Resume/Resume"
+import React from "react"
+
+const ResumePage = () => {
+    return (
+        <>
+            <h1>Resume page </h1>
+            <Resume />
+        </>
+    )
+}
+
+export default ResumePage
---
+++ src/types/ResumeTypes/index.ts
@@ -0,0 +1,3 @@
+export type { default as Resume} from './Resume'
+export type { default as LifeHighlights} from './LifeHighlights'
+export type { default as Skill} from './Skill'
