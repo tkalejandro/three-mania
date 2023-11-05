ad52839dd10ac873c19f5d0850bedd5558fc8763
J. Alejandro Coronado <tk.jalejandro@gmail.com>
2023-11-05 17:10:50 +0100

    next ui is concluded

--- src/modules/Resume/Resume.tsx
+++ src/modules/Resume/Resume.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem } from '@nextui-org/react';
 import { soniaCoronado } from '@/data';
 import { useAppSettings } from '@/store';
 import { Button } from '@nextui-org/react';
@@ -15,6 +15,19 @@ const Resume = () => {
       <div>
         <Button color="warning">Hello Button</Button>
       </div>
+      <Dropdown>
+        <DropdownTrigger>
+          <Button variant="bordered">Open Menu</Button>
+        </DropdownTrigger>
+        <DropdownMenu aria-label="Static Actions">
+          <DropdownItem key="new">New file</DropdownItem>
+          <DropdownItem key="copy">Copy link</DropdownItem>
+          <DropdownItem key="edit">Edit file</DropdownItem>
+          <DropdownItem key="delete" className="text-danger" color="danger">
+            Delete file
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
     </div>
   );
 };
