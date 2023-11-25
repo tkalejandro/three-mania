c2bd8081db04bded82e45493517273ead9e334ca
J. Alejandro Coronado <tk.jalejandro@gmail.com>
2023-11-25 20:18:09 +0100

    loading to chakrahtml

--- src/modules/Experience/scenes/LoaderScene/LoaderScene.tsx
+++ src/modules/Experience/scenes/LoaderScene/LoaderScene.tsx
@@ -1,6 +1,7 @@
 import { Html, useProgress } from '@react-three/drei';
 import React from 'react';
-
+import { ChakraHtml } from '../../components';
+import { Text, useTheme } from '@chakra-ui/react';
 /**
  * First Thing to appear :-)
  * This might dissapear in the future.
@@ -8,8 +9,12 @@ import React from 'react';
  */
 const LoaderScene = () => {
   const { active, progress, errors, item, loaded, total } = useProgress();
-
-  return <Html center>{progress.toFixed(0)} % loaded</Html>;
+  const theme = useTheme();
+  return (
+    <ChakraHtml center>
+      <Text color={theme.colors.white}>{progress.toFixed(0)} % loaded</Text>
+    </ChakraHtml>
+  );
 };
 
 export default LoaderScene;
