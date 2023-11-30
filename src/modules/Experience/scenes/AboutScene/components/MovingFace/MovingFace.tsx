4f02545e7308d837381dd011331c76a76a089c94
J. Alejandro Coronado <tk.jalejandro@gmail.com>
2023-11-30 22:08:29 +0100

    more leva control added to face.

--- src/modules/Experience/scenes/AboutScene/components/MovingFace/MovingFace.tsx
+++ src/modules/Experience/scenes/AboutScene/components/MovingFace/MovingFace.tsx
@@ -6,12 +6,15 @@ import { useControls } from 'leva';
 const MovingFace = () => {
   const faceRef = useRef<Mesh>(null!);
 
-  const { positionIntensityX, positionIntensityY, lookAtIntensity } = useControls('Face', {
-    positionIntensityX: { value: 1.1, step: 0.001, min: 0, max: 2 },
-    positionIntensityY: { value: 0.5, step: 0.001, min: 0, max: 2 },
-    lookAtIntensity: { value: 2, step: 0.001, min: 0, max: 10 },
-    position: { value: [0, 1, 0] },
-  });
+  const { positionIntensityX, positionIntensityY, lookAtIntensity, positionY } = useControls(
+    'Face',
+    {
+      positionIntensityX: { value: 1.1, step: 0.001, min: 0, max: 2 },
+      positionIntensityY: { value: 0.5, step: 0.001, min: 0, max: 2 },
+      lookAtIntensity: { value: 2, step: 0.001, min: 0, max: 10 },
+      positionY: { value: 2 },
+    },
+  );
 
   useFrame((state, delta) => {
     // Mouse coordinates from -1 to 1
@@ -23,7 +26,8 @@ const MovingFace = () => {
     // Update mesh position based on mouse position
     if (faceRef.current) {
       faceRef.current.position.x = mouseX * positionIntensityX;
-      faceRef.current.position.y = 1 + mouseY * positionIntensityY;
+      // PositionY is to correct the position of the face.
+      faceRef.current.position.y = positionY + mouseY * positionIntensityY;
 
       // Make the face look at the mouse
       faceRef.current.lookAt(mouseX * lookAtIntensity, mouseY * lookAtIntensity, Math.PI * 0.5);
@@ -31,7 +35,7 @@ const MovingFace = () => {
   });
 
   return (
-    <mesh ref={faceRef} position={[0, 1, 0]}>
+    <mesh ref={faceRef} position={[0, positionY, 0]}>
       <planeGeometry args={[2, 4]} />
       <meshStandardMaterial color={'pink'} />
     </mesh>
