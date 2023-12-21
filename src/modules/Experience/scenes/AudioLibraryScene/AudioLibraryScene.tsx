143565a69a173b711f694b5e5f0047a686f032a7
J. Alejandro Coronado <tk.jalejandro@gmail.com>
2023-12-21 17:45:52 +0100

    feat: audio library initiated

--- src/modules/Experience/scenes/AudioLibraryScene/AudioLibraryScene.tsx
+++ src/modules/Experience/scenes/AudioLibraryScene/AudioLibraryScene.tsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react';
 import { useFrame, Vector3 } from '@react-three/fiber';
 import { Text } from '@react-three/drei';
 import { AudioCover } from './components';
-import { useAppTheme } from '@/hooks';
+import { useAppBreakpoints, useAppTheme } from '@/hooks';
 
 interface AudioLibrarySceneProps {
   position: Vector3;
@@ -10,16 +10,31 @@ interface AudioLibrarySceneProps {
 
 const AudioLibraryScene = ({ position }: AudioLibrarySceneProps) => {
   const theme = useAppTheme();
+
+  // The hook is reactive
+  const { isTablet } = useAppBreakpoints();
+
+  let margin = 1;
+  switch (true) {
+    case isTablet: {
+      margin = 2;
+      break;
+    }
+    default:
+      margin = 1;
+  }
+
   return (
     <group position={position}>
-      <AudioCover position={[-0.5, 0, 0]} color={theme.colors.secondary[500]} />
-      <AudioCover position={[0, 0, 0]} color={theme.colors.primary[200]} />
-      {/* <AudioCover position={[-1, 0, 0]} color={theme.colors.primary[200]} />
-      <AudioCover position={[-1, 0, 0]} color={theme.colors.primary[200]} />
-      <AudioCover position={[-1, 0, 0]} color={theme.colors.primary[200]} /> */}
+      <AudioCover position={[-1 * margin, 0, -1]} color={theme.colors.secondary[500]} />
+      <AudioCover position={[-0.5 * margin, 0, -0.5]} color={theme.colors.primary[300]} />
+      <AudioCover position={[0 * margin, 0, 0]} color={theme.colors.warning[300]} />
+      <AudioCover position={[0.5 * margin, 0, -0.5]} color={theme.colors.danger[300]} />
+      <AudioCover position={[1 * margin, 0, -1]} color={theme.colors.success[400]} />
 
-      <Text position={[0, -1, 0]} scale={0.2}>
-        Im Audio Library
+      {/* //TODO - Should be a blender model?? */}
+      <Text position={[0, -1.1, 0]} scale={0.2} color={theme.colors.primary.main}>
+        Play
       </Text>
     </group>
   );
