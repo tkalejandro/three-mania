2608d92e04651eb8cbf9bfbfe95dfecf39d78781
J. Alejandro Coronado <tk.jalejandro@gmail.com>
2024-01-03 18:11:58 +0100

    SEO was added to the project.

--- src/app/developers/page.tsx
+++ src/app/developers/page.tsx
@@ -1,12 +1,34 @@
 import { Box } from '@chakra-ui/react';
 import { Developers } from '@/modules/StaticPages/Developers/Developers';
 import React from 'react';
+import Head from 'next/head';
 
 const DevelopersPage = () => {
   return (
-    <Box>
-      <Developers />
-    </Box>
+    <>
+      <Head>
+        <title>Meet the Developers | Portfolio Sonia Coronado</title>
+        <meta
+          name="description"
+          content="Explore the talented developers behind this Portfolio Sonia Coronado. Learn about their skills, expertise, and contributions."
+          key="desc"
+        />
+        <meta property="og:title" content="Meet the Developers | Portfolio Sonia Coronado" />
+        <meta
+          property="og:description"
+          content="Explore the talented developers behind Portfolio Sonia Coronado. Learn about their skills, expertise, and contributions."
+        />
+        <meta
+          property="og:image"
+          content="https://media.licdn.com/dms/image/C5603AQFFkALXGQDtfA/profile-displayphoto-shrink_400_400/0/1598900964195?e=1709769600&v=beta&t=wL_1M0FfPLkAc-oEt_q9-m1GknjK3MCYfxe6KV1tog8"
+        />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://example.com/developers" />
+      </Head>
+      <Box>
+        <Developers />
+      </Box>
+    </>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
 import Experience from '@/modules/Experience/Experience';
+import Head from 'next/head';
 import React from 'react';
 import { Providers } from './providers';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Sonia Coronado - Music Portfolio</title>
+        <meta
+          name="description"
+          content="Immerse yourself in Sonia Coronado's captivating music portfolio. Explore a unique blend of creativity, innovation, and musical mastery."
+          key="desc"
+        />
+        <meta property="og:title" content="Sonia Coronado - Music Portfolio" />
+        <meta
+          property="og:description"
+          content="Immerse yourself in Sonia Coronado's captivating music portfolio. Explore a unique blend of creativity, innovation, and musical mastery."
+        />
+        <meta
+          property="og:image"
+          content="https://media.licdn.com/dms/image/C5603AQFFkALXGQDtfA/profile-displayphoto-shrink_800_800/0/1598900964195?e=1709769600&v=beta&t=rxBatjiyd0-pUfSojY_vv9PW5oJmP7AVQ2249wSisfg"
+        />
+        <meta name="robots" content="index, follow" />
+        <link rel="canonical" href="https://example.com" />
+      </Head>
       <Experience />
     </>
   );
