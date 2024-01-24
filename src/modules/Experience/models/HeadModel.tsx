de5f06d016d7f7ce97066b833d875160e442d608
Hicham2012 <99765449+Hicham2012@users.noreply.github.com>
2024-01-24 12:07:37 +0100

    MODIFY: use gltfScene to get head geometry

--- src/modules/Experience/models/HeadModel.tsx
+++ src/modules/Experience/models/HeadModel.tsx
@@ -13,20 +13,20 @@ interface HeadModelProps extends GroupProps {
 const HeadModel = ({ selectedColor, map, ...props }: HeadModelProps) => {
   const [gltfScene, setGltfScene] = useState<Group<Object3DEventMap> | null>(null);
   const { scene } = useGLTF('/models/head-3.glb')
-  /**
-   * Model created by @Hicham2012 and
-   * inspired from this tutorial ↓
-   * https://youtu.be/AlPPYkZg9D4?si=L_chsWglPCB2DAGk
-   */
-  // Let's ignore the issue for now
-  // @ts-ignore
-  const head = scene.children[0].geometry.clone()
-
+  
   useEffect(() => {
     setGltfScene(scene)
   }, [])
   const cachedModel = useMemo(() => {
     if (gltfScene) {
+      /**
+       * Model created by @Hicham2012 and
+       * inspired from this tutorial ↓
+       * https://youtu.be/AlPPYkZg9D4?si=L_chsWglPCB2DAGk
+       */
+      // Let's ignore the issue for now
+      // @ts-ignore
+      const head = gltfScene.children[0].geometry.clone()
       return (
         <group
           {...props}
