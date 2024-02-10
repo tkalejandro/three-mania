ab703b91fe2139e9c4dbe21a794c4c85e1426fc7
J. Alejandro Coronado <tk.jalejandro@gmail.com>
2024-02-10 16:41:05 +0100

    clean code

--- src/modules/Experience/components/EnhancedImage/EnhancedImage.tsx
+++ src/modules/Experience/components/EnhancedImage/EnhancedImage.tsx
@@ -1,8 +1,7 @@
 import { fontLibrary } from '@/helpers';
 import { Image, ImageProps, Text } from '@react-three/drei';
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import { Vector3 } from 'three'; // Import Vector3 from three.js library
-import * as THREE from 'three';
 import { useAppBreakpoints } from '@/hooks';
 type EnhancedImageProps = ImageProps & {
   caption: string;
@@ -11,8 +10,8 @@ type EnhancedImageProps = ImageProps & {
 const EnhancedImage = ({ caption, ...props }: EnhancedImageProps) => {
   const { position, scale } = props;
   const { isBigTablet } = useAppBreakpoints();
-  // Destructure position from props
-  if (!position || !(position instanceof Vector3) || !scale) return null; // Check if position exists and is a Vector3 object
+  // Check if position exists and is a Vector3 object
+  if (!position || !(position instanceof Vector3) || !scale) return null; 
 
   const imageY = typeof scale === 'number' ? scale : scale[1] ?? 0;
 
@@ -25,6 +24,7 @@ const EnhancedImage = ({ caption, ...props }: EnhancedImageProps) => {
         maxWidth={15}
         font={fontLibrary.montserrat.bold}
         scale={textSize}
+        // Divide by 2 because is half of the image. and  65% of it we will use.  The lower the higher the text
         position={[position.x, position.y - (imageY / 2) * 0.65, position.z + 0.001]}
       >
         {caption}
