dcb6bf940df4a53cfb2dece6978849c2c6b27100
J. Alejandro Coronado <tk.jalejandro@gmail.com>
2024-02-12 13:10:03 +0100

    Infinite scroll working as expected.

--- src/modules/Experience/scenes/AudioLibraryScene/AudioLibraryScene.tsx
+++ src/modules/Experience/scenes/AudioLibraryScene/AudioLibraryScene.tsx
@@ -1,39 +1,139 @@
-import React, { useRef, useState } from 'react';
-import { useFrame, Vector3 } from '@react-three/fiber';
+import React, { useEffect, useRef, useState } from 'react';
+import { useFrame, useThree, Vector3 as VectorR3f } from '@react-three/fiber';
 import { Text } from '@react-three/drei';
 import { AudioCover } from './components';
 import { useAppBreakpoints, useAppTheme } from '@/hooks';
+import { Group, Vector3 } from 'three';
+import { InvisibleMesh } from '../../components';
 
 interface AudioLibrarySceneProps {
-  position: Vector3;
+  position: VectorR3f;
 }
+
+const zPoint = 1.0;
 
 const AudioLibraryScene = ({ position }: AudioLibrarySceneProps) => {
   const theme = useAppTheme();
+  const [isTouchEnabled, setIsTouchEnabled] = useState(false);
+  const { isBigTablet, isDesktop } = useAppBreakpoints();
+  const audioListRef = useRef<Group>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [initialMousePosition, setInitialMousePosition] = useState({ x: 0, y: 0 });
+  const [cameraRotation, setCameraRotation] = useState({ x: 0, y: 0 });
 
-  // The hook is reactive
-  const { isTablet } = useAppBreakpoints();
+  const { size } = useThree();
+  const screenWidth = size.width / 100;
 
-  let margin = 1;
-  switch (true) {
-    case isTablet: {
-      margin = 2;
-      break;
+  useEffect(() => {
+    const disableTouch = (event: any) => {
+      event.preventDefault();
+    };
+    // Trick to make possible to simulate touch in mobile.
+    if (!isDesktop && isTouchEnabled) {
+      document.addEventListener('touchmove', disableTouch, { passive: false });
+      return () => {
+        document.removeEventListener('touchmove', disableTouch);
+      };
     }
-    default:
-      margin = 1;
-  }
+  }, [isDesktop, isTouchEnabled]);
+
+  const handlePointerDown = (event: {
+    stopPropagation: () => void;
+    clientX: any;
+    clientY: any;
+  }) => {
+    event.stopPropagation();
+    setIsDragging(true);
+    setInitialMousePosition({ x: event.clientX, y: event.clientY });
+  };
+
+  const handlePointerMove = (event: { clientX: number; clientY: number }) => {
+    if (!isDragging || !isTouchEnabled) return;
+
+    const deltaX = event.clientX - initialMousePosition.x;
+    const deltaY = event.clientY - initialMousePosition.y;
+
+    setCameraRotation({
+      x: cameraRotation.x + deltaY * 0.005,
+      y: cameraRotation.y + deltaX * -0.005,
+    });
+
+    setInitialMousePosition({ x: event.clientX, y: event.clientY });
+  };
+
+  const dragFalse = () => {
+    setIsDragging(false);
+  };
+
+  const xRadius = isDesktop ? 1.75 : isBigTablet ? 1.5 : 1;
+  const zRadius = isDesktop ? 0.75 : isBigTablet ? 0.65 : 0.65;
+  const calculateCoverPositions = (numCovers: number, rotationAngle: number) => {
+    const positions = [];
+    const angleIncrement = (2 * Math.PI) / numCovers;
+
+    for (let i = 0; i < numCovers; i++) {
+      const angle = rotationAngle + i * angleIncrement;
+      const x = Math.cos(angle) * zPoint * xRadius; // Radius of 1.2 units
+      const z = Math.sin(angle) * zPoint * zRadius; // Radius of 1.2 units
+      positions.push(new Vector3(x, 0, z));
+    }
+
+    return positions;
+  };
+
+  const audioCovers = [
+    {
+      name: '1',
+    },
+    {
+      name: '2',
+    },
+    {
+      name: '3',
+    },
+    {
+      name: '4',
+    },
+    {
+      name: '5',
+    },
+  ];
+
+  const rotationAngle = Math.PI / 2 + cameraRotation.y; // Adjusting for initial rotation
+  const coverPositions = calculateCoverPositions(audioCovers.length, rotationAngle);
 
   return (
     <group position={position}>
-      <AudioCover position={[-1 * margin, 0, -1]} color={theme.colors.secondary[500]} />
-      <AudioCover position={[-0.5 * margin, 0, -0.5]} color={theme.colors.primary[300]} />
-      <AudioCover position={[0 * margin, 0, 0]} color={theme.colors.warning[300]} />
-      <AudioCover position={[0.5 * margin, 0, -0.5]} color={theme.colors.danger[300]} />
-      <AudioCover position={[1 * margin, 0, -1]} color={theme.colors.success[400]} />
+      {/* Render audio covers with dynamic positions */}
+      <InvisibleMesh
+        //onPointerOver={handlePointerMove}
+        onPointerMove={handlePointerMove}
+        onPointerDown={handlePointerDown}
+        onPointerUp={dragFalse}
+        onPointerLeave={dragFalse}
+        invisibleSize={[screenWidth, 2, 0]}
+        invisiblePosition={[0, 0, 0]}
+      >
+        <group position={[0, 0, -zPoint]} ref={audioListRef}>
+          {audioCovers.map((cover, index) => (
+            <AudioCover
+              key={index}
+              position={coverPositions[index].toArray()}
+              color={theme.colors.secondary[500]}
+            />
+          ))}
+        </group>
+      </InvisibleMesh>
 
-      {/* //TODO - Should be a blender model?? */}
-      <Text position={[0, -1.1, 0]} scale={0.2} color={theme.colors.primary.main}>
+      {/* Text */}
+      <Text
+        position={[0, -1.1, 0]}
+        scale={0.2}
+        color={theme.colors.primary.main}
+        onClick={() => {
+          setIsTouchEnabled(!isTouchEnabled);
+        }}
+      >
         Play
       </Text>
     </group>
